0ede258d2724c988542eaade11f49c47ac618e6a
fady <fady@email.com>
2024-09-13 18:15:24 +0300

    gui and tui

---
+++ catsay-gui-glade/src/main.rs
@@ -0,0 +1,36 @@
+use gtk::prelude::*;
+
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("layout.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    let window: gtk::Window = builder.object("applicationwindow1").unwrap();
+    let message_input: gtk::Entry = builder.object("message_input").unwrap();
+    let button: gtk::Button = builder.object("generate_btn").unwrap();
+    let message_output: gtk::Label = builder.object("message_output").unwrap();
+    let image_output: gtk::Image = builder.object("image_output").unwrap();
+    let image_output_clone = image_output.clone();
+    let is_dead_switch: gtk::Switch = builder.object("is_dead_switch").unwrap();
+
+    button.connect_clicked(move |_| {
+        let is_dead = is_dead_switch.is_active();
+        if is_dead {
+            image_output_clone.set_from_file(Some("./images/cat_dead.png"))
+        } else {
+            image_output_clone.set_from_file(Some("./images/cat.png"))
+        }
+        message_output.set_text(&format!("{}\n \\\n  \\", message_input.text().as_str()));
+        image_output_clone.show();
+    });
+
+    window.set_application(Some(app));
+    window.show_all();
+    image_output.hide();
+}
+
+fn main() {
+    let application = gtk::Application::new(Some("com.catsay-gui-glade"), Default::default());
+    application.connect_activate(build_ui);
+
+    application.run();
+}
